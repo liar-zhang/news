e25d6162549ff94062d7be286b09e19433742362
Your Name <you@example.com>
2020-05-06 18:08:59 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<style lang="less">
+    // 公共样式
+    *{
+      margin:0;
+      padding: 0;
+    }
+    a{
+      color:inherit;
+      text-decoration: none;
+    }
+    a:hover{
+      color:inherit;
+    }
+    ul,ol,li{
+      list-style: none;
+    }
+    i,em{
+      font-style:normal;
+    }
+</style>
